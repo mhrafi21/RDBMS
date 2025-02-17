dad86caa10ee486eb87654e97d3142acc4f06f9c
mhrafi21 <12mhrafi@gmail.com>
2025-02-18 02:00:31 +0600

    more add query

--- module-51/alter.sql
+++ module-51/alter.sql
@@ -4,4 +4,15 @@ SELECT * FROM person2
 -- inset into db
 INSERT INTO person2 values(2, 'MAHDI', 22, 'RAFI@GAMIL.COM')
 
--- 
+-- add column in the existing table 
+ALTER TABLE person2 
+    ADD COLUMN phone INTEGER DEFAULT '0193959694' NOT NULL;
+
+-- delete existing table column name;
+
+ALTER TABLE person2
+     DROP phone;
+
+-- rename column field name;
+ALTER TABLE person2
+      RENAME age to user_age;
